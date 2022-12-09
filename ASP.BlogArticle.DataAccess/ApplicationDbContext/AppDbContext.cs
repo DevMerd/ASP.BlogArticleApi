6094fc69e8a250c8dd9f4af8ac916a097707a813
Mustafa Mert Kaya <1030510047@erciyes.edu.tr>
2022-12-09 08:07:13 +0300

    Add project files.

---
+++ ASP.BlogArticle.DataAccess/ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ASP.BlogArticle.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.BlogArticle.DataAccess.ApplicationDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+           
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MERT;Database=ASP.BlogArticleDb;User Id=mert;Password=sapass; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ ASP.BlogArticle/Controllers/BlogArticleController.cs
@@ -0,0 +1,65 @@
+﻿using ASP.BlogArticle.Core.Models;
+using ASP.BlogArticle.DataAccess.ApplicationDbContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ASP.BlogArticle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogArticleController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public BlogArticleController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Article>> GetArticlesAsync()
+        {
+            var articles = await _appDbContext.Articles.ToListAsync();
+            return articles;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Article>> GetArticlesById(int id)
+        {
+            var article = await _appDbContext.Articles.FindAsync(id);
+            if (article == null)
+                return BadRequest("Article not found");
+
+            return Ok(article);
+        }
+
+        [HttpPost]
+        public async Task<IEnumerable<Article>> CreateArticle(Article article)
+        {
+            _appDbContext.Add(article);
+            await _appDbContext.SaveChangesAsync();
+            var articles = await _appDbContext.Articles.ToListAsync();
+            return articles;
+        }
+
+        [HttpPut]
+        public void Put(int id, [FromBody] Article article)
+        {
+            if (id != article.Id)
+                throw new ArgumentException($"Article with {id} doesn't exist !");
+            _appDbContext.Entry(article).State = EntityState.Modified;
+            _appDbContext.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var article = _appDbContext.Articles.SingleOrDefault(a=>a.Id==id);
+            if (article == null)
+                throw new ArgumentException($"Article with {id} doesn't exist !");
+            _appDbContext.Remove(article);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
